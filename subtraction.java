4ef441076239b2b504afda9776502954568efe6c
Amit <sahuamit00786>
2023-04-19 00:16:28 -0700

    subtraction

---
+++ subtraction.java
@@ -0,0 +1,10 @@
+public class sum
+{
+    public static void main(String[] args)
+    {
+        int a=9;
+        int b =0;
+        System.out.println(a-b);
+        System.out.println("hello");
+    }
+}
